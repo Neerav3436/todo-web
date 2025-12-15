d29eaa08fb87086b04366bcb1bb6b914692fcc12
neerav3436 <neeravpatel85@gmail.com>
2025-12-14 21:56:28 -0500

    Add local picture upload feature

--- pages/home.py
+++ pages/home.py
@@ -8,10 +8,21 @@ camera_image = None
 with sl.expander("Click to open camera"):
     camera_image = sl.camera_input("Take a picture", key="camera")
 
+uploaded_image = None
+with sl.expander("Upload picture"):
+    uploaded_image = sl.file_uploader("Upload an image", type=["png", "jpg", "jpeg"], key="picture")
+
 sl.write(sl.session_state)
 
 if(camera_image is not None):
     img = Image.open(camera_image)
+
+    gray_scale = img.convert("L")
+
+    sl.image(gray_scale)
+
+if(uploaded_image is not None):
+    img = Image.open(uploaded_image)
 
     gray_scale = img.convert("L")
 
