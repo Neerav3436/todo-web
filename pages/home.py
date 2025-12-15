c5887cde7552652a9479bdbf37bd1a4c67400ceb
neerav3436 <neeravpatel85@gmail.com>
2025-12-14 21:44:03 -0500

    camera feature

--- pages/home.py
+++ pages/home.py
@@ -1,3 +1,18 @@
 import streamlit as sl
+from PIL import Image 
+#Pillow already part of streamlit
 
 sl.title("This is Home Page")
+
+camera_image = None
+with sl.expander("Click to open camera"):
+    camera_image = sl.camera_input("Take a picture", key="camera")
+
+sl.write(sl.session_state)
+
+if(camera_image is not None):
+    img = Image.open(camera_image)
+
+    gray_scale = img.convert("L")
+
+    sl.image(gray_scale)
